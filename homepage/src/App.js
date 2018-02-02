18ca96a97ba4bbc66714d22f8af85368eda4bfca
Ryan Dunton <ryan.dunton@area23hc.com>
2018-02-02 15:53:37 -0500

    added header row

--- homepage/src/App.js
+++ homepage/src/App.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-
-import ProjectRow from './components/project_row.js'
+import HeaderRow from './components/header_row'
+import ProjectRow from './components/project_row'
 
 class App extends Component {
   render() {
     return (
       <div className="container-fluid">
+        <HeaderRow mainImage="media/ny.jpg" headshot="media/headshot.png" />
         <ProjectRow name="Weather Forecaster (React)" link="weather" image="media/weather.jpg"/>
         <ProjectRow name="Ryantube! (YT-ish Clone Using React)" link="ryantube" image="media/ryantube.jpg"/>
         <ProjectRow name="Todo List App (React)" link="todo" image="media/todo.png"/>
