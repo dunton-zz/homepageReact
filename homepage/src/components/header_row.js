9b7b888b10d86fd13cf7ee13b03bcb4fb7584f0c
Ryan Dunton <ryandunton1@gmail.com>
2018-10-03 18:39:10 -0400

    stash

--- homepage/src/components/header_row.js
+++ homepage/src/components/header_row.js
@@ -1,23 +1,39 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class HeaderRow extends Component {
-	render() {
-		return (
-			<div className="col-md-12 header">
-				<img className="img-responsive" src={this.props.mainImage} alt="main background image" />
-					<div id="headshot">
-						<div id ="headPic">
-							<img src={this.props.headshot} alt="headshot" />
-						</div>
-						<h1 id="name">Ryan Dunton</h1>
-						<h3 id = "position">Front-End Developer</h3>
-						<h4 id ="position2">Front-End Developer</h4>
-						<p id="info">Experienced in building with technologies such as <span id="tech">Javascript, HTML/CSS, React, Bootstrap, and various APIs including Google Maps, Foursquare and Yelp</span>.</p>
-						<p id ="info">Below are links to some of my work, my resume and my contact information. <br/> I also built and deployed this website from scratch. Enjoy!</p>
-					</div>
-			</div>
-		)
-	}
+  render() {
+    return (
+      <div className="row">
+        <div className="col-md-12 header">
+          <img
+            className="img-responsive"
+            src={this.props.mainImage}
+            alt="main background image"
+          />
+          <div id="headshot">
+            <div id="headPic">
+              <img src={this.props.headshot} alt="headshot" />
+            </div>
+            <h1 id="name">Ryan Dunton</h1>
+            <h3 id="position">Front-End Developer</h3>
+            <h4 id="position2">Front-End Developer</h4>
+            <p id="info">
+              Experienced in building with technologies such as{" "}
+              <span id="tech">
+                Javascript, HTML/CSS, React, Bootstrap, and various APIs
+                including Google Maps, Foursquare and Yelp
+              </span>.
+            </p>
+            <p id="info">
+              Below are links to some of my work, my resume and my contact
+              information. <br /> I also built and deployed this website from
+              scratch. Enjoy!
+            </p>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default HeaderRow;
--- homepage/src/components/project_row.js
+++ homepage/src/components/project_row.js
@@ -1,20 +1,24 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class ProjectRow extends Component {
-	render() {
-		return (
-			<div className="row">
-				<div className="col-md-12">
-					<div id ={this.props.name} className="mainPic">
-						<a href={this.props.link + ".html" }>
-							<img className="img-responsive" src={this.props.image} />
-							<h2>{this.props.name}</h2>
-						</a>
-					</div>
-				</div>
-			</div>
-		)
-	}
+  render() {
+    return (
+      <div className="row">
+        <div className="col-md-12">
+          <div id={this.props.name} className="mainPic">
+            <a href={this.props.link}>
+              <img
+                className="img-responsive"
+                src={this.props.image}
+                alt="project image"
+              />
+              <h2>{this.props.name}</h2>
+            </a>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default ProjectRow;
